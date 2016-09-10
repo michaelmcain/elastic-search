57253d38953d364a71b5453cc9660105b370e4d8
Michael Cain <michaelmcain@yahoo.com>
2016-09-10 17:16:04 -0400

    fuuuuuuck

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+
   resources :articles
+  get "search", to: "search#search"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
